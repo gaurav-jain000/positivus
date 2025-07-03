af6da19cb297e8a96cd65fc722fc5adbeb0b0df9
gaurav-jain000 <116086566+gaurav-jain000@users.noreply.github.com>
2025-07-03 10:25:21 +0530

    Add shared.ts for shared data, Add Social Links component, Add Footer, Update Header, Update Layout to add Footer component

---
+++ src/lib/shared.ts
@@ -0,0 +1,7 @@
+export const navLinksArr = [
+  { title: "About us", url: "/" },
+  { title: "Services", url: "/" },
+  { title: "Use Cases", url: "/" },
+  { title: "Pricing", url: "/" },
+  { title: "Blog", url: "/" },
+];
